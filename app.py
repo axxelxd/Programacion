1b8f68b94c785f7afaa124fcee1d7b5ad9b30a2d
Axel Cevallos <axelstevcevallosromero@gmail.com>
2026-03-30 23:20:52 -0500

    actualice la pagina

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
-#Crea una función que reciba una palabra y retorne la cadena invertida.
+import tkinter as tk
 
-def invertir(palabra):
-    return palabra[:: -1]
+# Función para calcular el IMC
+def calcular_imc():
+    try:
+        peso = float(entrada_peso.get())
+        altura = float(entrada_altura.get())
 
-texto = input("ingresa una palabra: ")
+        imc = peso / (altura ** 2)
 
-resultado = invertir(texto)
+        resultado.config(text=f"IMC: {imc:.2f}")
 
-print("palabra invertida:", resultado)
+        # Clasificación
+        if imc < 18.5:
+            estado.config(text="Bajo peso")
+        elif imc < 25:
+            estado.config(text="Peso normal")
+        elif imc < 30:
+            estado.config(text="Sobrepeso")
+        else:
+            estado.config(text="Obesidad")
+
+    except:
+        resultado.config(text="Error: datos inválidos")
+        estado.config(text="")
+
+ventana = tk.Tk()
+ventana.title("Calculadora de IMC")
+ventana.geometry("300x250")
+
+tk.Label(ventana, text="Peso (kg):").pack()
+entrada_peso = tk.Entry(ventana)
+entrada_peso.pack()
+
+tk.Label(ventana, text="Altura (m):").pack()
+entrada_altura = tk.Entry(ventana)
+entrada_altura.pack()
+
+tk.Button(ventana, text="Calcular IMC", command=calcular_imc).pack()
+
+resultado = tk.Label(ventana, text="")
+resultado.pack()
+
+estado = tk.Label(ventana, text="")
+estado.pack()
+
+ventana.mainloop()
